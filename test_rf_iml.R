ad62fdd707c5c493704cd8432917d9a3456c019b
Giuseppe Casalicchio <giuseppe.casalicchio@stat.uni-muenchen.de>
2025-11-21 15:27:03 +0100

    Enhance prediction output with print and CSV export
    Added result printing and CSV export for predictions.

--- test_rf_iml.R
+++ test_rf_iml.R
@@ -1,9 +1,15 @@
 library(randomForest)
 library(iml)
 set.seed(78546)
-X = subset(iris, select = -Species)[-130L,]
-y = iris$Species[-130L]
-rf = randomForest(X, y, ntree = 20L)
-predictor = iml::Predictor$new(rf, data = iris[-130L, ], y = "Species", type = "prob")
-x_interest = iris[130L, ]
-predictor$predict(x_interest)
+X <- subset(iris, select = -Species)[-130L, ]
+y <- iris$Species[-130L]
+rf <- randomForest(X, y, ntree = 20L)
+predictor <- iml::Predictor$new(rf,
+                                 data = iris[-130L, ],
+                                 y = "Species",
+                                 type = "prob")
+x_interest <- iris[130L, ]
+result <- predictor$predict(x_interest)
+print(result)
+write.csv(result, file = "prediction_result.csv", row.names = FALSE)
+sessionInfo()
